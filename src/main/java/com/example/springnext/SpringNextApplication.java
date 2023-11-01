ebbfa322d5eb0325a7590d732fe0e7810fec7b98
kristianlive <kristianlive@hotmail.com>
2023-11-01 13:58:38 +0100

    frist commit

---
+++ src/main/java/com/example/springnext/SpringNextApplication.java
@@ -0,0 +1,13 @@
+package com.example.springnext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringNextApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringNextApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springnext/SpringNextApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springnext;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringNextApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
