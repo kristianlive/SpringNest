8ee049b7c5a279ed6f8864d9a787dc71a578ca7d
kristianlive <kristianlive@hotmail.com>
2023-11-09 12:56:03 +0100

    Add homecontroller

--- src/main/java/com/example/springnext/controller/HomeController.java
+++ src/main/java/com/example/springnext/controller/HomeController.java
@@ -1,7 +1,8 @@
 package com.example.springnext.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HomeController {
     @GetMapping("/home")
     public String home() {
