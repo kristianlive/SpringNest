b020adbfb466a420b5ee19df633129b4c2c94d45
kristianlive <kristianlive@hotmail.com>
2023-11-09 13:41:48 +0100

    Added Login funktion with success and after move on to /home sucess

--- src/main/java/com/example/springnext/controller/HomeController.java
+++ src/main/java/com/example/springnext/controller/HomeController.java
@@ -1,11 +1,18 @@
 package com.example.springnext.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
 @Controller
 public class HomeController {
+
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model, Principal principal) {
+        String username = principal != null ? principal.getName() : "Guest";
+        model.addAttribute("user", username);
         return "home";
     }
 }
